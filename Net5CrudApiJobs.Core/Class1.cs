394987b9e19ee4f5825685099aca13cca526e257
AlejandroRicotti <Alejandro.Ricotti@hotmail.com>
2022-02-13 02:15:13 -0300

    Agregar archivos de proyecto.

---
+++ Net5CrudApiJobs.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Net5CrudApiJobs.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Net5CrudApiJobs.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Net5CrudApiJobs.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
